0088168bb43c4a2eb27dcdc911e3da60d2a8721b
dori35 <dori.nagy35@gmail.com>
2022-12-18 07:08:07 +0100

    podium

--- frontend/src/state/startTest/selectors.js
+++ frontend/src/state/startTest/selectors.js
@@ -1,2 +1,3 @@
 export const getStartTest = (state) => state.startTest;
 export const getResults = (state) => state.startTest.results;
+export const getTopThree = (state) => state.startTest.results.topThree;
--- frontend/src/views/start/Finish.js
+++ frontend/src/views/start/Finish.js
@@ -1,16 +1,29 @@
 import { useEffect, useState } from "react";
-import { Card, Container } from "react-bootstrap";
+import {
+  Card,
+  Col,
+  Container,
+  Dropdown,
+  DropdownButton,
+  Row,
+} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { getIsLoggedIn, getUserId } from "../../state/auth/selectors";
+import { getIsLoggedIn, getToken, getUserId } from "../../state/auth/selectors";
 import { fetchResults } from "../../state/startTest/actions";
-import { getResults } from "../../state/startTest/selectors";
+import {
+  getResults,
+  getTopThree,
+  gettopThree,
+} from "../../state/startTest/selectors";
+import "./Finish.css";
 
 export function Finish() {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(getIsLoggedIn);
   const userId = useSelector(getUserId);
   const results = useSelector(getResults);
+  const topThree = useSelector(getTopThree);
   const [processing, setProcessing] = useState(true);
   const { url } = useParams();
 
@@ -33,26 +46,120 @@ export function Finish() {
 
   return (
     <>
-      {isLoggedIn && (
-        <div className="d-flex justify-content-center align-items-center">
-          <Container className="py-5">
-            <div className="d-flex justify-content-center">
-              <Card>
-                <Card.Body>
-                  <Card.Title> Eremények</Card.Title>
-                  {processing ? (
-                    <Card.Text> Feldolgozás alatt</Card.Text>
-                  ) : (
-                    <Card.Text>
-                      {results.currentPoints} / {results.maxPoints}
+      <div className="d-flex justify-content-center align-items-center ">
+        <Container className="py-5">
+          <div className="d-flex justify-content-center">
+            <Card className="alma">
+              <Card.Body>
+                <Card.Title> Eremények</Card.Title>
+                {processing ? (
+                  <Card.Text> Feldolgozás alatt</Card.Text>
+                ) : (
+                  <>
+                    <Card.Text className=" pt-4 pb-0 mb-0">
+                      A Te pontszámod:{" "}
+                      <span style={{ fontWeight: "bold" }}>
+                        {results.currentPoints}
+                      </span>
                     </Card.Text>
-                  )}
-                </Card.Body>
-              </Card>
-            </div>
-          </Container>
-        </div>
-      )}
+                    <Card.Text className=" pt-1 ">
+                      Maximum pontszám:
+                      <span style={{ fontWeight: "bold" }}>
+                        {results.maxPoints}
+                      </span>
+                    </Card.Text>
+                    <Col className="  pb-4 ">
+                      <Row>
+                        <Col className="  mxy-0 px-0 mt-3 text-center  ">
+                          <span
+                            style={{ fontWeight: "bold" }}
+                            className=" align-items-center"
+                          >
+                            2.
+                          </span>
+                          <Col className=" mxy-0 pxy-0 pb-2  bg-warning  ">
+                            <DropdownButton
+                              variant="warning"
+                              menuVariant="dark"
+                              id="second"
+                              title={
+                                !!topThree[1]
+                                  ? `${topThree[1].points} pont`
+                                  : "- pont"
+                              }
+                              disabled={!topThree[1]}
+                            >
+                              {!!topThree[1]
+                                ? topThree[1].identifiers.map((identifier) => (
+                                    <Dropdown.Item>{identifier}</Dropdown.Item>
+                                  ))
+                                : null}
+                            </DropdownButton>
+                          </Col>
+                        </Col>
+                        <Col className=" mxy-0 px-0 mt-2 text-center">
+                          <span
+                            style={{ fontWeight: "bold" }}
+                            className=" align-items-center"
+                          >
+                            1.
+                          </span>
+                          <Col className=" mxy-0 pxy-0 pb-3 bg-warning  ">
+                            <DropdownButton
+                              variant="warning"
+                              menuVariant="dark"
+                              id="first"
+                              title={
+                                !!topThree[0]
+                                  ? `${topThree[0].points} pont`
+                                  : "- pont"
+                              }
+                              disabled={!topThree[0]}
+                            >
+                              {!!topThree[0]
+                                ? topThree[0].identifiers.map((identifier) => (
+                                    <Dropdown.Item>{identifier}</Dropdown.Item>
+                                  ))
+                                : null}
+                            </DropdownButton>
+                          </Col>
+                        </Col>
+                        <Col className=" mxy-0 px-0 mt-4 text-center">
+                          <span
+                            style={{ fontWeight: "bold" }}
+                            className=" align-items-center"
+                          >
+                            3.
+                          </span>
+                          <Col className=" mxy-0 pxy-0 bg-warning ">
+                            <DropdownButton
+                              variant="warning"
+                              menuVariant="dark"
+                              id="third"
+                              title={
+                                !!topThree[2]
+                                  ? `${topThree[2].points} pont`
+                                  : "- pont"
+                              }
+                              disabled={!topThree[2]}
+                            >
+                              {!!topThree[2]
+                                ? topThree[2].identifiers.map((identifier) => (
+                                    <Dropdown.Item>{identifier}</Dropdown.Item>
+                                  ))
+                                : null}
+                            </DropdownButton>
+                          </Col>{" "}
+                        </Col>
+                      </Row>
+                    </Col>
+                  </>
+                )}
+              </Card.Body>
+            </Card>
+          </div>
+        </Container>
+      </div>
     </>
   );
 }
